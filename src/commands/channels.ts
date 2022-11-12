0bb4cad2c39050d5d788597467a3d74aca646af8
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-12 19:18:43 +0100

    Added "channels" command

---
+++ src/commands/channels.ts
@@ -0,0 +1,29 @@
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
+import { channelNames, Rarety } from "../util";
+import { ExtendedClient, ICommand } from "../bot";
+
+export const command: ICommand = {
+    data: new SlashCommandBuilder()
+        .setName("channels")
+        .setDescription("Le taux de drop des noms de channel"),
+    async execute(client: ExtendedClient, interaction: CommandInteraction): Promise<void> {
+        const legendaryNames: string = channelNames.legendary.toString().split(',').join("\n");
+        const epicNames: string = channelNames.epic.toString().split(',').join("\n");
+        const rareNames: string = channelNames.rare.toString().split(',').join("\n");
+        const communNames: string = channelNames.commun.toString().split(',').join("\n");
+        const embed: EmbedBuilder = new EmbedBuilder()
+            .setTitle("Taux de drop des noms")
+            .setColor("Blue")
+            .setThumbnail(client.user!.displayAvatarURL())
+            .addFields(
+                { name: `Légendaire (${Rarety.Legendary}%)`, value: legendaryNames },
+                { name: `Epique (${Rarety.Epic}%)`, value: epicNames },
+                { name: `Rare (${Rarety.Rare}%)`, value: rareNames },
+                { name: `Commun (${Rarety.Commun}%)`, value: communNames }
+            );
+
+        await interaction.followUp({ embeds: [embed] });
+    }
+}
+
+export default command;
