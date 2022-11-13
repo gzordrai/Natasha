41eb6ecd1b589374091fbc5b6930273ab7ecdde6
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-13 05:39:21 +0100

    Added "profile" command

---
+++ src/commands/profile.ts
@@ -0,0 +1,32 @@
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { Cooldown, User } from "../database";
+import { ExtendedClient, ICommand } from "../bot";
+
+export const command: ICommand = {
+    data: new SlashCommandBuilder()
+        .setName("profile")
+        .setDescription("Votre profil"),
+    async execute(client: ExtendedClient, interaction: CommandInteraction): Promise<void> {
+        const user: User = await new User(interaction.user.id).sync();
+        let cooldowns = "";
+        user.cooldowns.forEach((cooldown: Cooldown, key: string) => {
+            cooldowns += `${key}: `;
+
+            if(cooldown.isFinished())
+                cooldowns += "Ready !\n";
+            else
+                cooldowns += cooldown.getTimeLeft() + '\n';
+        });
+        const embed: EmbedBuilder = new EmbedBuilder()
+            .setTitle(`Porfil de ${interaction.user.username}`)
+            .setThumbnail(interaction.user.displayAvatarURL())
+            .addFields(
+                { name: "solde", value: `${user.balance.get()} ${interaction.guild?.emojis.cache.get(process.env.PETAL_EMOJI_ID!)}`},
+                { name: "cooldowns", value: cooldowns}
+            )
+
+        await interaction.followUp({ embeds: [embed] });
+    }
+}
+
+export default command;
