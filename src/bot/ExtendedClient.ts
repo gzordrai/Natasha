4f946f8850dee3801bed5972a682d4967be10b9e
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-13 02:31:31 +0100

    Adding a database system

--- src/bot/ExtendedClient.ts
+++ src/bot/ExtendedClient.ts
@@ -1,6 +1,8 @@
 import { Client, Collection } from "discord.js";
+import { Database } from "../util/Database";
 import { ICommand } from ".";
 
 export class ExtendedClient extends Client {
     public commands: Collection<string, ICommand> = new Collection<string, ICommand>;
+    public database: Database = new Database("../../data/database.json");
 }
