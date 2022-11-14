08c649230a2b8e026034f560874ce8d53bd2e0a4
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-14 01:06:56 +0100

    Added channel verification for commands

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -6,8 +6,12 @@ const event: IEvent = {
     name: Events.InteractionCreate,
     once: false,
     async execute(client: ExtendedClient, interaction: Interaction): Promise<void> {
-        if (interaction.isCommand())
-            await handleSlashCommand(client, interaction);
+        if (interaction.isCommand()) {
+            if (interaction.channelId === process.env.BOT_COMMAND_CHANNEL_ID! || interaction.user.id === process.env.OWNER_ID!)
+                await handleSlashCommand(client, interaction);
+            else
+                await interaction.reply({ content: "Mauvais channel abruti !" });
+        }
     },
 };
 
