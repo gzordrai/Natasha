72bfddfd8d959ddabd082dd87bd69c415f6a7d3b
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-14 19:57:26 +0100

    Added reward when someone get a legendary channel

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,6 +1,7 @@
-import { ChannelType, Events, VoiceChannel, VoiceState } from "discord.js";
-import { randomChannelName } from "../util/randomChannelName";
+import { ChannelType, Events, GuildBasedChannel, GuildEmoji, VoiceChannel, VoiceState } from "discord.js";
+import { channelNames, randomChannelName } from "../util/randomChannelName";
 import { ExtendedClient, IEvent } from "../bot";
+import { User } from "../database";
 
 const event: IEvent = {
     name: Events.VoiceStateUpdate,
@@ -8,12 +9,24 @@ const event: IEvent = {
     async execute(client: ExtendedClient, oldState: VoiceState, newState: VoiceState): Promise<void> {
         // Voice connection
         if (newState.channelId === process.env.CHANNEL_CREATOR_ID) {
+            const user: User = await new User(newState.member?.id!).sync();
+            const name: string = await randomChannelName();
+
             await newState.guild.channels.create({
-                name: await randomChannelName(),
+                name: name,
                 type: ChannelType.GuildVoice
             }).then(async (channel: VoiceChannel) => {
                 channel.setParent(newState.channel!.parentId);
                 newState.setChannel(channel.id);
+
+                if (channelNames.legendary.includes(name)) {
+                    const guildChannel: GuildBasedChannel =  newState.guild.channels.cache.get(process.env.BOT_COMMAND_CHANNEL_ID!)!;
+
+                    if(guildChannel.isTextBased()) {
+                        guildChannel.send({ content: `${newState.member?.user} a eu un channel légendaire ! Il gagne donc 50 ${newState.guild?.emojis.cache.get(process.env.PETAL_EMOJI_ID!)!}` });
+                        user.balance.add(50);
+                    }
+                }
             }).catch(console.error);
         } else if (oldState.channelId !== null) {// Voice disconnection
             if (oldState.channel!.members.size === 0 && oldState.channelId !== process.env.CHANNEL_CREATOR_ID)
