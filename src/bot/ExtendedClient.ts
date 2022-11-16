150596122801781e6dfed8bf1ac323f384dd0257
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-16 21:36:28 +0100

    Added missing parentheses

--- src/bot/ExtendedClient.ts
+++ src/bot/ExtendedClient.ts
@@ -2,5 +2,5 @@ import { Client, Collection } from "discord.js";
 import { Command } from ".";
 
 export class ExtendedClient extends Client {
-    public commands: Collection<string, Command> = new Collection<string, Command>;
+    public commands: Collection<string, Command> = new Collection<string, Command>();
 }
