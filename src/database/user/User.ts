b6821a3568ecc3c0754ec5c59409b2df08a6dc0f
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-18 15:34:52 +0100

    Reworked User class

---
+++ src/database/user/User.ts
@@ -0,0 +1,43 @@
+import { Collection } from "discord.js";
+import { Balance, Cooldown, Database, Inventory, Collectable, Tool } from "../";
+import { JSONCooldown, JSONUser } from "../JSON";
+
+export class User {
+    private id: string;
+    public balance: Balance;
+    public cooldowns: Collection<string, Cooldown>;
+    public inventory: Inventory;
+
+    public constructor(id: string, balance: Balance, cooldowns: Collection<string, Cooldown>, inventory: Inventory) {
+        this.id = id;
+        this.balance = balance;
+        this.cooldowns = cooldowns;
+        this.inventory = inventory;
+    }
+
+    /**
+     * The id of the user
+     * 
+     * @returns user id
+     */
+    public getId(): string {
+        return this.id;
+    }
+
+    /**
+     * The data of the class intended to be stored in a .json
+     * 
+     * @returns class data
+     */
+    public toJSON(): JSONUser {
+        const balance: number = this.balance.get();
+        let cooldowns: JSONCooldown = {};
+
+        for(const cooldown of this.cooldowns.values())
+            cooldowns[cooldown.getName()] = cooldown.getStart();
+
+        console.log(cooldowns);
+
+        return { balance: balance, cooldowns: cooldowns, inventory: this.inventory.toJSON() };
+    }
+}
--- src/database/user/index.ts
+++ src/database/user/index.ts
@@ -1,3 +1,4 @@
 export * from "./Balance";
 export * from "./Cooldown";
 export * from "./Inventory";
+export * from "./User";
