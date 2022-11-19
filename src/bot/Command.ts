a1a4215016c229148c33e053630495f482b2ca22
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-19 20:34:34 +0100

    Added the user who launches the command as a parameter

--- src/bot/Command.ts
+++ src/bot/Command.ts
@@ -1,8 +1,9 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
 import { ExtendedClient } from ".";
+import { User } from "../database";
 
 export interface Command {
     data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
     cooldown?: number;
-    execute: (client: ExtendedClient, interaction: ChatInputCommandInteraction) => Promise<void>;
+    execute: (client: ExtendedClient, interaction: ChatInputCommandInteraction, user: User) => Promise<void>;
 }
