e4feaa695c382007755300fa2b84872b4f1dc724
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-20 03:17:46 +0100

    Added tools and collectables

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -1,5 +1,5 @@
 import { ChatInputCommandInteraction, EmbedBuilder, GuildEmoji, SlashCommandBuilder } from "discord.js";
-import { User } from "../database";
+import { Collectable, Tool, User } from "../database";
 import { ExtendedClient, Command } from "../bot";
 
 export const command: Command = {
@@ -9,13 +9,27 @@ export const command: Command = {
     async execute(client: ExtendedClient, interaction: ChatInputCommandInteraction, user: User): Promise<void> {
         const embed: EmbedBuilder = new EmbedBuilder();
         const petalEmoji: GuildEmoji = interaction.guild!.emojis.cache.get(process.env.PETAL_EMOJI_ID!)!;
+        let tools: string = "- ";
+        let collectables: string = "- ";
+
+        user.inventory.getTools().forEach((tool: Tool) => {
+            const emoji: GuildEmoji = interaction.guild?.emojis.cache.get(process.env[`${tool.getName()}_EMOJI_ID`]!)!;
+
+            tools += `x${tool.getCopies()} ${emoji}\n`;
+        });
+
+        user.inventory.getCollectables().forEach((collectable: Collectable) => {
+            const emoji: GuildEmoji = interaction.guild?.emojis.cache.get(process.env[`${collectable.getName()}_EMOJI_ID`]!)!;
+
+            collectables += `x${collectable.getCopies()} ${emoji}\n`;
+        });
 
         embed.setTitle(`Profil de ${interaction.user.username}`);
         embed.setThumbnail(interaction.user.displayAvatarURL());
         embed.addFields(
             { name: "Solde:", value: `${user.balance.get()} ${petalEmoji}` },
-            { name: "Outils:", value: "work in progress... 👀" },
-            { name: "Collection:", value: "work in progress... 👀" }
+            { name: "Outils:", value: tools },
+            { name: "Collection:", value: collectables }
         );
 
         await interaction.followUp({ embeds: [embed] });
